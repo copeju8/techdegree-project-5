3b19c6ee593e15b2e34a627370f156dc7debb0ec
copeju8 <51244110+copeju8@users.noreply.github.com>
2019-07-19 09:56:15 -0400

    Update script

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+/*Goal: Build an app for a fictional company called Awesome Startup, a distributed company with
+remote employees working all over world.  They need a smart way for employees to share contact information
+with each other.*/
+
+/*Approach:
+Use the Random User Generator (API) to grab information for 12 random "employees," and use that data
+to build a prototype for an Awesome Startup Employee directory.*/
+
+/*Result:
+Ability to request a JSON object from the API and parse the data so that 12 employees are listed in the
+grid with their thumbnail image, full name, email, and location.   Clicking the employee's image or name will
+open a modal window with more detailed information, such as the employee's birthday and address.*/
