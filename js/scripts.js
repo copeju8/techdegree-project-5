395c8fb89337cc206e72cffb76213b5a5cbc2e07
copeju8 <51244110+copeju8@users.noreply.github.com>
2019-07-22 16:27:47 -0400

    Updates button Issues

--- js/scripts.js
+++ js/scripts.js
@@ -27,7 +27,17 @@ fetch(url)                                                      //Fetch method r
 
 function generateData(data) {
 
+
     data.results.map(person => {
+
+        $('#gallery').on('click', '.card', function (event) {
+            //i = ($(this).index());
+            i = ($(event.target).index());
+            //modalWindow(i);
+            generateModal(person);
+        });
+
+
         let card = document.createElement('div');
         card.classList.add('card');
         card.innerHTML = `
@@ -41,95 +51,70 @@ function generateData(data) {
                 <p class="card-text cap">${person.location.city}<p> 
         </div>    
         `;
-        $('#gallery').on('click', '.card', function (event) {
-            //i = ($(this).index());
-            i = ($(event.target).index());
-            modalWindow(i);
-            generateModal(person);
-            console.log(i);
-        });
-        function modalWindow(i) {
-            if (i === 0) {
-                $('.modal-prev').remove()
-            } else if (i === 11) {
-                $('.modal-next').remove()
-            }
-        }
-        $('.modal-next').on('click', function () {
-            $('.modal-container').remove();
-            i++
-            modalWindow(i);
-        });
-
-        $('.modal-prev').on('click', function () {
-            $('.modal-container').remove();
-            i--
-            modalWindow(i);
-        })
 
         $('#gallery').append(card);
     })
+
 }
 
 function generateModal(person) { //Generate card profiles using the employee(person) data from generateData.
     //Sets the markup to the HTML defined in the template literal using passed in data
 
 
-
-
-    // const modal = document.querySelector(".modal");
-    // const trigger = document.querySelector(".trigger");
-    // const closeButton = document.querySelector(".modal-close-btn")
-
-    // function toggleModal() {
-    //     modal.classList.toggle("modal");
-    // }
-
-    // function windowOnClick(event) {
-    //     if (event.target === modal) {
-    //         toggleModal();
+    // function modalWindow(i) {
+    //     if (i === 0) {
+    //         $('.modal-prev').remove()
+    //     } else if (i === 11) {
+    //         $('.modal-next').remove()
     //     }
     // }
-    // trigger.addEventListener('click', toggleModal);
-    // closeButton.addEventListener('click', toggleModal);
-    // window.addEventListener('click, windowOnClick');
+    // $('.modal-next').on('click', function () {
+    //     $('.modal-container').remove();
+    //     i++
+    //     generateModal(person);
+    // });
+
+
+    // $('.modal-prev').on('click', function () {
+    //     $('.modal-container').remove();
+    //     i--
+    //     generateModal(person);
+    // })
 
     let dob = person.dob.date;
     let bDay = dob.slice(5, 7) + '/' + dob.slice(8, 10) + '/' + dob.slice(0, 4);
 
     const modalHtml = `
-            <div class="modal-container">
-                <div class="modal">
-                    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong></strong></button>
-                </div>  
-
-                <div class="modal-info-container">
-                    <img class="modal-img" src=${person.picture.large} alt="profile picture"> 
+       
+    <div class="modal-container">
+        <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src= ${person.picture.large} alt="profile picture">
                     <h3 id="name" class="modal-name cap">${person.name.first} ${person.name.last}</h3>
                     <p class="modal-text">${person.email}</p>
                     <p class="modal-text cap">${person.location.city}</p>
                     <hr>
-                    <p class="modal-text">${person.cell}</p>
-                    <p class="modal-text">${person.location.street}, ${person.location.city}, ${person.location.state} ${person.location.postcode}</p>
-                    <p class="modal-text">Birthday: ${bDay}</p> 
+                        <p class="modal-text">${person.cell}</p>
+                        <p class="modal-text">${person.location.street}, ${person.location.city}, ${person.location.state} ${person.location.postcode}</p>
+                        <p class="modal-text">Birthday: ${bDay}</p>
+                    </div>
                 </div>
-                 <div class="modal-btn-container">
+
+                <div class="modal-btn-container">
                     <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
                     <button type="button" id="modal-next" class="modal-next btn">Next</button>
                 </div>
-            </div>
-            </div> `;
+            </div>`
+
     $('body').append(modalHtml);
+
+    $('#modal-close-btn').on('click', function () {
+        $('.modal-container').remove();
+        console.log('hello');
+    });
 };
 
-
-// function checkStatus() {
-//     if(response.ok) {
-//       return Promise.resolve(response);
-//     } else {
-//       return Promise.reject(new Error(response.statusText));
-//     } 
-//   }
 
 // //Post Data
 
