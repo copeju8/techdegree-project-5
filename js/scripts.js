5eed88201fd915a147d5ca10905c08f8f65a83b1
copeju8 <51244110+copeju8@users.noreply.github.com>
2019-07-22 09:03:32 -0400

    Updates 7_22 AM

--- js/scripts.js
+++ js/scripts.js
@@ -12,60 +12,58 @@ const url = ('https://randomuser.me/api/?results=12&nat=US');   //Futue option f
 const card = document.getElementById('.card');           //Retrieves all employee data     
 const pictures = document.getElementById('.card-img');            //Retrieves employee pictures (images)
 const info = document.querySelector(".card-info-container");    //Retrieves employee information
-const btn = document.createElement('button');                   
 
 /*Fetch Functions*/
-    
-    fetch(url)                                                   //Fetch method replaces the AJAX request                         
+
+fetch(url)                                                   //Fetch method replaces the AJAX request                         
     .then(response => response.json())                           //Raw data (parsed) in the JSON format - returns a promise                     
     .then(data => generateData(data))                           //Getting actual JSON data                                                        
-    //.catch(error => console.log(" Something went wrong! Re-enter your request!"))                
+//.catch(error => console.log(" Something went wrong! Re-enter your request!"))                
 
 // /*Helper Functions*/
 
-function generateData(data)    {                                    //Generate the markup for each employee profile
-                                                                    //Sets the markup to the HTML defined in the template literal using passed in data
-    const html = data.results.map( person => `
-
+function generateData(data) {                                    //Generate the markup for each employee profile
+    //Sets the markup to the HTML defined in the template literal using passed in data
+    data.results.map(person => {
+        const html = `
+        <div class="card">
         <div class="card-img-container">
                 <img class ="card-img" src=${person.picture.large} alt="profile picture">
-            </div>
+        </div>
             
             <div class="card-info-container">
                 <h3 id="name" class="card-name cap">${person.name.first} ${person.name.last}</h3>
                 <p class="card-text">${person.email}<p>
                 <p class="card-text cap">${person.location.city}<p>
             </div>  
-        `) .join ('')
-
- $('.gallery').append(html);    
+        `;
+        $('#gallery').append(html);
+    })
 }
 
-function generateModal(data)    {                                    //Generate the markup for each employee profile
-                                                                    //Sets the markup to the HTML defined in the template literal using passed in data
-     //let formatDOB = str.reverse
-     const modalHtml = data.results.map( person => `
+function generateModal(data) {      //Generate the markup for each employee profile
+    //Sets the markup to the HTML defined in the template literal using passed in data
+    //let formatDOB = str.reverse
+    data.results.map(person => {
+        const modalHtml = `
+        <div class="modal-container">
+        <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong></strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src=${person.picture.large} alt="profile picture"> 
+                <h3 id="name" class="modal-name cap">${person.name.first} ${person.name.last}</h3>
+                <p class="modal-text">${person.email}</p>
+                <p class="modal-text cap">${person.location.city}</p>
+                <hr>
+                <p class="modal-text">${person.cell}</p>
+                <p class="modal-text">${person.location.street}+','+ ${person.location.city} +','+ ${person.location.state} ${person.location.postcode}</p>
+                <p class="modal-text">Birthday: ${person.dob.date}</p> 
+            </div>
+        </div> `;
+    })
 
-    <div class="modal-container">
-    <div class="modal">
-        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>${person.btn}</strong></button>
-        <div class="modal-info-container">
-            <img class="modal-img" src=${person.picture.large} alt="profile picture"> 
-            <h3 id="name" class="modal-name cap">${person.name.first} ${person.name.last}</h3>
-            <p class="modal-text">${person.email}</p>
-            <p class="modal-text cap">${person.location.city}</p>
-            <hr>
-            <p class="modal-text">${person.cell}</p>
-            <p class="modal-text">${person.location.street}+','+ ${person.location.city} +','+ ${person.location.state} ${person.location.postcode}</p>
-            <p class="modal-text">Birthday: ${person.dob.date}</p> 
-        </div>
-    </div>
-        `) .join ('')
-
-$('.modal-container').append(modalHtml);    
-}
-
-
+    $('#gallery').append(modalHtml);
+};
 // function checkStatus() {
 //     if(response.ok) {
 //       return Promise.resolve(response);
@@ -76,7 +74,11 @@ $('.modal-container').append(modalHtml);
 
 
 // //Event Listeners
-card.addEventListener('click', generateModal);
+
+// $('#gallery').on('click', () => {
+//     generateModal();
+//     console.log('hello');
+// });
 
 
 
@@ -86,3 +88,13 @@ card.addEventListener('click', generateModal);
 // Ability to request a JSON object from the API and parse the data so that 12 employees are listed in the
 // grid with their thumbnail image, full name, email, and location.   Clicking the employee's image or name will
 // open a modal window with more detailed information, such as the employee's birthday and address.*/
+
+
+// $("modal-close-btn").on('click', function () {
+        //     $("modal-container").remove();
+        // });
+
+        // $('#gallery').on('click', '.card', function () {
+        //     i = ($(this).index());
+        //     generateModal(i);
+        // });
